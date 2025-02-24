5401c86fe43a71ec624fd6e0cb9228384cbae4dc
Alex <olexandrpasalsky@gmail.com>
2025-02-24 12:11:15 +0200

    add halfstep mode

--- Lab1/Unipolar/src/main.cpp
+++ Lab1/Unipolar/src/main.cpp
@@ -8,6 +8,8 @@
 #define STEPUP (PINC & (1 << PINC2))
 #define STEPDOWN (PINC & (1 << PINC3))
 
+#define HALFSTEPMODE (PINC & (1 << PINC5))
+
 
     // Ініціалізація змінних
 int checkUp = 1; 
@@ -16,7 +18,17 @@ int angle = 0;
 int stepAngle = 90;
 int timeDelay = 100;
 
-    // Функція зміни значення порту Е, порт виводу керуючого впливу на двигун в залежності від кута
+
+// Функція ініціалізації портів С та Е
+void init_ports()
+{
+    DDRC = 0x00;
+    DDRE = 0xFF; // порт С на введення; порт Е на виведення
+}
+
+
+    // Функція зміни значення порту Е, для крокового режиму,
+    // порт виводу керуючого впливу на двигун в залежності від кута 
 void stepper() 
 {
     if (angle == 90)
@@ -39,11 +51,201 @@ void stepper()
         PORTE = 0x09;
 }
 
-    // Функція ініціалізації портів С та Е
-void init_ports()
+//Функція керування МК для роботи уніполярного двигуна в кроковому режимі
+void step_mode()
 {
-    DDRC = 0x00;
-    DDRE = 0xFF; // порт С на введення; порт Е на виведення
+    // Якщо натиснута кнопка STEPUPAUTO (PC0) двигун обертається за годинниковою стрілкою
+    if (!STEPUPAUTO) 
+    {
+        PORTE = 0x0C;
+        _delay_ms(timeDelay);
+        PORTE = 0x06;           // 0x06 = 0b00000110  (PE1 і PE2 = 1)
+        _delay_ms(timeDelay);
+        PORTE = 0x03;
+        _delay_ms(timeDelay);
+        PORTE = 0x09;
+        _delay_ms(timeDelay);
+    }
+    
+        // Якщо натиснута кнопка STEPDOWNAUTO (PC1) двигун обертається проти годинникової стрілки
+    if (!STEPDOWNAUTO) 
+    {
+        PORTE = 0x03;
+        _delay_ms(timeDelay);
+        PORTE = 0x06;
+        _delay_ms(timeDelay);
+        PORTE = 0x0C;
+        _delay_ms(timeDelay);
+        PORTE = 0x09;
+        _delay_ms(timeDelay);
+    }
+    
+        // Виконати крок за годинниковою стрілкою, якщо натиснута кнопка STEPUP (PC2), 
+        // змінити значення змінної angle на +90 та викликати функцію stepper()
+    if (!STEPUP && checkUp == 1) 
+    {
+        if (angle == 360)
+        {
+            angle = 0;
+        }
+        angle += stepAngle;
+        checkUp = 0;
+    }
+    
+        // Якщо кнопка відпущена STEPUP (PC2)
+    if (STEPUP) 
+    {
+        checkUp = 1; // 18. Прапорець checkUp встановлюється в 1
+    }
+    
+        // Виконати крок проти годинникової стрілки, якщо натиснута кнопка STEPDOWN (PC3),
+        // змінити значення змінної angle на -90 та викликати функцію stepper()
+    if (!STEPDOWN && checkDown == 1) 
+    {
+        if (angle == -360)
+        {
+            angle = 0;
+        }
+        angle -= stepAngle;
+        checkDown = 0;
+    }
+    
+        // Якщо кнопка відпущена STEPDOWN (PC3)
+    if (STEPDOWN) 
+    {
+        checkDown = 1;
+    }
+    
+        // Виклик функції зміни кута повороту двигуна в покроковому режимі
+    stepper();
+
+}
+
+
+    // Функція зміни значення порту Е, для напівкрокового режиму,
+    // порт виводу керуючого впливу на двигун в залежності від кута
+void half_stepper()
+{
+    if (angle == 0)
+        PORTE = 0x09;
+    if (angle == 45)
+        PORTE = 0x08;
+    if (angle == 90)
+        PORTE = 0x0;
+    if (angle == 135)
+        PORTE = 0x04;
+    if (angle == 180)
+        PORTE = 0x06;
+    if (angle == 225)
+        PORTE = 0x02;
+    if (angle == 270)
+        PORTE = 0x03;
+    if (angle == 315)
+        PORTE = 0x01;
+    if (angle == 360)
+        PORTE = 0x09;
+    if (angle == -45)
+        PORTE = 0x01;
+    if (angle == -90)
+        PORTE = 0x03;
+    if (angle == -135)
+        PORTE = 0x02;
+    if (angle == -180)
+        PORTE = 0x06;
+    if (angle == -225)
+        PORTE = 0x04;
+    if (angle == -270)
+        PORTE = 0x0C;
+    if (angle == -315)
+        PORTE = 0x08;
+    if (angle == -360)
+        PORTE = 0x09;
+}
+
+    //Функція керування МК для роботи уніполярного двигуна в напівкроковому режимі
+void half_step_mode()
+{
+    // Якщо натиснута кнопка STEPUPAUTO (PC0) двигун обертається за годинниковою стрілкою
+    if (!STEPUPAUTO) 
+    {
+        PORTE = 0x08;       // 0x08 = 0b00001010  (PE1 і PE3 = 1)
+        _delay_ms(timeDelay);
+        PORTE = 0x0C;
+        _delay_ms(timeDelay);
+        PORTE = 0x04;
+        _delay_ms(timeDelay);
+        PORTE = 0x06;
+        _delay_ms(timeDelay);
+        PORTE = 0x02;
+        _delay_ms(timeDelay);
+        PORTE = 0x03;
+        _delay_ms(timeDelay);
+        PORTE = 0x01;
+        _delay_ms(timeDelay);
+        PORTE = 0x09;
+        //_delay_ms(timeDelay);
+    }
+    
+        // Якщо натиснута кнопка STEPDOWNAUTO (PC1) двигун обертається проти годинникової стрілки
+    if (!STEPDOWNAUTO) 
+    {
+        PORTE = 0x01;
+        _delay_ms(timeDelay);
+        PORTE = 0x03;
+        _delay_ms(timeDelay);
+        PORTE = 0x02;
+        _delay_ms(timeDelay);
+        PORTE = 0x06;
+        _delay_ms(timeDelay);
+        PORTE = 0x04;
+        _delay_ms(timeDelay);
+        PORTE = 0x0C;
+        _delay_ms(timeDelay);
+        PORTE = 0x08;
+        _delay_ms(timeDelay);
+        PORTE = 0x09;
+        //_delay_ms(timeDelay);
+    }
+    
+        // Виконати крок за годинниковою стрілкою, якщо натиснута кнопка STEPUP (PC2), 
+        // змінити значення змінної angle на +90 та викликати функцію stepper()
+    if (!STEPUP && checkUp == 1) 
+    {
+        if (angle == 360)
+        {
+            angle = 0;
+        }
+        angle += stepAngle;
+        checkUp = 0;
+    }
+    
+        // Якщо кнопка відпущена STEPUP (PC2)
+    if (STEPUP) 
+    {
+        checkUp = 1; // 18. Прапорець checkUp встановлюється в 1
+    }
+    
+        // Виконати крок проти годинникової стрілки, якщо натиснута кнопка STEPDOWN (PC3),
+        // змінити значення змінної angle на -90 та викликати функцію stepper()
+    if (!STEPDOWN && checkDown == 1) 
+    {
+        if (angle == -360)
+        {
+            angle = 0;
+        }
+        angle -= stepAngle;
+        checkDown = 0;
+    }
+    
+        // Якщо кнопка відпущена STEPDOWN (PC3)
+    if (STEPDOWN) 
+    {
+        checkDown = 1;
+    }
+    
+        // Виклик функції зміни кута повороту двигуна в покроковому режимі
+    half_stepper();
+
 }
 
 
@@ -53,70 +255,17 @@ int main(void)
     
     while (1)
     {
-
-            // Якщо натиснута кнопка STEPUPAUTO (PC0) двигун обертається за годинниковою стрілкою
-        if (!STEPUPAUTO) 
+        // Якщо відпущена кнопка HALFSTEPMODE (PC5) двигун працює в кроковому режимі,
+        // інакше в напівкроковому режимі
+        if(HALFSTEPMODE)
         {
-            PORTE = 0x0C;
-            _delay_ms(timeDelay);
-            PORTE = 0x06;           // 0x06 = 0b00000110  (PE1 і PE2 = 1)
-            _delay_ms(timeDelay);
-            PORTE = 0x03;
-            _delay_ms(timeDelay);
-            PORTE = 0x09;
-            _delay_ms(timeDelay);
+            stepAngle = 90;
+            step_mode();
         }
-        
-            // Якщо натиснута кнопка STEPDOWNAUTO (PC1) двигун обертається проти годинникової стрілки
-        if (!STEPDOWNAUTO) 
+        else
         {
-            PORTE = 0x03;
-            _delay_ms(timeDelay);
-            PORTE = 0x06;
-            _delay_ms(timeDelay);
-            PORTE = 0x0C;
-            _delay_ms(timeDelay);
-            PORTE = 0x09;
-            _delay_ms(timeDelay);
+            stepAngle = 45;
+            half_step_mode();
         }
-        
-            // Виконати крок за годинниковою стрілкою, якщо натиснута кнопка STEPUP (PC2), 
-            // змінити значення змінної angle на +90 та викликати функцію stepper()
-        if (!STEPUP && checkUp == 1) 
-        {
-            if (angle == 360)
-            {
-                angle = 0;
-            }
-            angle += stepAngle;
-            checkUp = 0;
-        }
-        
-            // Якщо кнопка відпущена STEPUP (PC2)
-        if (STEPUP) 
-        {
-            checkUp = 1; // 18. Прапорець checkUp встановлюється в 1
-        }
-        
-            // Виконати крок проти годинникової стрілки, якщо натиснута кнопка STEPDOWN (PC3),
-            // змінити значення змінної angle на -90 та викликати функцію stepper()
-        if (!STEPDOWN && checkDown == 1) 
-        {
-            if (angle == -360)
-            {
-                angle = 0;
-            }
-            angle -= stepAngle;
-            checkDown = 0;
-        }
-        
-            // Якщо кнопка відпущена STEPDOWN (PC3)
-        if (STEPDOWN) 
-        {
-            checkDown = 1;
-        }
-        
-            // Виклик функції зміни кута повороту двигуна в покроковому режимі
-        stepper();
     }
 }
